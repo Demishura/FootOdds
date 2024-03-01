bea4d4c0d98f49fca9fe39827777607742e9ec36
Kevin Perez <kperez2298@gmail.com>
2024-03-01 00:17:30 -0700

    Commit inicial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Cabecera from './Header/Header';
+import NavBar from './NavBar/NavBar';
+import LastMatches from './LastMatches/LastMatches';
+import NextMatches from './NextMatches/NextMatches';
+import OddCalculator from './OddCalculator/OddCalculator';
+import ImageGallery from './Gallery/Gallery';
+import TemporaryDrawer from './Drawe/Drawer';
+import 'tailwindcss/tailwind.css'
+import { Route,Routes } from 'react-router-dom';
+import Resultados from './Laliga/Resultados';
+import Clasificacion from './Laliga/Clasificacion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Cabecera/>
+      <TemporaryDrawer/>
+      <NavBar/>
+      <div className='flex flex-nowrap max-sm:flex-col'>
+      <Routes>
+        <Route path='/' element={<><ImageGallery/><LastMatches/></>}/>
+        <Route path='/Laliga/Resultados' element={<Resultados/>}/>
+        <Route path='/Laliga/Clasificacion' element={<Clasificacion/>}/>
+      </Routes>
+      </div>
+      
+      
     </div>
   );
 }
